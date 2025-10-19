af14afdc39e89049ce9510ceebd8faa7eb05c304
Connor Mills <connor@virtualized.dev>
2025-10-18 23:45:18 -0700

    Explicit importing/exporting

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ mod display;
 mod models;
 mod parser;
 
-pub use display::*;
-pub use models::*;
-pub use parser::*;
+pub use display::{print_body_information, print_header_information};
+pub use models::{Body, Header};
+pub use parser::split_content;
